ca0314e920e1228279a3b3231709a4b8b72005b5
Renan Vasconcelos Lima <renanvlima1@gmail.com>
2018-11-17 15:32:16 -0200

    commit leitura matriz
    oraganizacao projeto e leitura dos 2 tipos de matrizes

--- Algoritmo Genético/src/GATSP.java
+++ Algoritmo Genético/src/GATSP.java
@@ -9,36 +9,26 @@ public class GATSP {
 	private static final String endereco = "Instancias GA";
 
 	private static Integer n = 0;
- 
 
 	public static void main(String[] args) {
 		try {
+			String nomeArquivo ;
 			BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-			// System.out.println("Insira o endereço da pagina: ");
-			// private static final String endereco = "C:\\Users\\yago\\Desktop\\tgw162";
-			// String endereco = br.readLine();
-			System.out.println("Insira o nome do arquivo de dados (ex.: dados.txt):");
-
-			String nomeArquivo;
-
 			nomeArquivo = "gr17.tsp";/* input.readLine(); */
-			n = identificarTamanhoMatriz(nomeArquivo);
-			// while ((new File(endereco+"\\"+nomeArquivo)).exists()) {
-			// System.out.println("Arquivo nÃ£o encontrado!\nInsira um novo nome para o
-			// arquivo: ");
-			// nomeArquivo = br.readLine();
-			// }
 			BufferedReader arquivoLeitura = new BufferedReader(new FileReader(endereco + "\\" + nomeArquivo));
-
 			String linha = arquivoLeitura.readLine();
 			String SomaLinhas = "";
 			ArrayList<Integer> elementosMatriz = new ArrayList<Integer>();
-			int contLinha = 1, i = 0, j = 0;
+			int contLinha = 1;
+			
+			System.out.println("Insira o nome do arquivo de dados (ex.: dados.txt):");
+			
+			n = identificarTamanhoMatriz(nomeArquivo);
 			while (linha != null) {
 				String[] elemento = linha.trim().split(" ");
 				if (contLinha > 7) {
 					for (int x = 0; x < elemento.length; x++) {
-						if(elemento[x].equals("EOF")) {
+						if (elemento[x].equals("EOF")) {
 							break;
 						}
 						elementosMatriz.add(Integer.parseInt(elemento[x]));
@@ -48,29 +38,9 @@ public class GATSP {
 				contLinha++;
 				linha = arquivoLeitura.readLine();
 			}
-			Integer[][] matrizDist = new Integer[n][n];
-			for(int k=0;k < elementosMatriz.size();k++){
-				    matrizDist[i][j] = elementosMatriz.get(k);
-				    matrizDist[j][i] = elementosMatriz.get(k);
-				    j++;
-				    if(elementosMatriz.get(k)==0) {
-				    	i++;
-				    	j=0;
-				    }
-				    if( i==n-2 && j==n-1) {
-				    	break;
-				    }
-			}
-
-			for(int a=0; a<n; a++){
-				System.out.println();
-				for(int b=0; b<n; b++){
-						
-						System.out.print(matrizDist[a][b]+" ");
-				}			
-			}		
+			ManipuladorMatriz mm = new ManipuladorMatriz(n);
+			mm.montarMatriz(elementosMatriz,nomeArquivo);	
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -87,13 +57,11 @@ public class GATSP {
 			tamanhoString += nomeArquivo.charAt(i);
 		}
 		tamanho = Integer.parseInt(tamanhoString);
-		// System.out.println(tamanho);
 		return tamanho;
 	}
-	
-	public ArrayList<Integer>  tsp(int[][] matriz ) {
+
+	public ArrayList<Integer> tsp(int[][] matriz) {
 		ArrayList<Integer> melhoresElementos = new ArrayList<Integer>();
-		
 		return melhoresElementos;
 	}
 
---
+++ Algoritmo Genético/src/ManipuladorMatriz.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class ManipuladorMatriz {
+
+	public ManipuladorMatriz(Integer n) {
+		this.n =n;
+	}
+	
+	private  Integer n = 0;
+	
+	public  Integer identificarTamanhoMatriz(String nomeArquivo) {
+		Integer tamanho = 0;
+		String tamanhoString = "";
+		char elementoString;
+		for (int i = 2; i <= nomeArquivo.length(); i++) {
+			elementoString = nomeArquivo.charAt(i);
+			if (elementoString == '.') {
+				break;
+			}
+			tamanhoString += nomeArquivo.charAt(i);
+		}
+		tamanho = Integer.parseInt(tamanhoString);
+		// System.out.println(tamanho);
+		return tamanho;
+	}
+
+	public static String identificarTipoMatriz(String nomeArquivo) {
+		String tipo = "";
+		if (nomeArquivo.substring(0, 2).equals("gr")) {
+			tipo = "LOWER";
+		} else {
+			tipo = "UPPER";
+		}
+		return tipo;
+	}
+
+	public  Integer[][] montarMatriz(ArrayList<Integer> elementosMatriz,String nomeArquivo) {
+		String tipo = identificarTipoMatriz(nomeArquivo);
+		int i = 0, j = 0;
+		Integer[][] matrizDist = new Integer[n][n];
+		if (tipo.equals("LOWER")) {
+			for (int k = 0; k < elementosMatriz.size(); k++) {
+				matrizDist[i][j] = elementosMatriz.get(k);
+				matrizDist[j][i] = elementosMatriz.get(k);
+				j++;
+				if (elementosMatriz.get(k) == 0) {
+					i++;
+					j = 0;
+				}
+				if (i == n - 2 && j == n - 1) {
+					break;
+				}
+			}
+		}else if (tipo.equals("UPPER")){
+			for (int k = 0; k < elementosMatriz.size(); k++) {
+				if (elementosMatriz.get(k) == 0 && k!=0) {
+					i++;
+					j=i;
+				}
+				if (j == n) {
+					break;
+				}
+				matrizDist[i][j] = elementosMatriz.get(k);
+				matrizDist[j][i] = elementosMatriz.get(k);
+				j++;
+				
+			}
+		}
+		
+		for (int a = 0; a < n; a++) {
+			System.out.println();
+			for (int b = 0; b < n; b++) {
+
+				System.out.print(matrizDist[a][b] + " ");
+			}
+		}
+
+		return matrizDist;
+	}
+
+}
